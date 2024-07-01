5464d574d77e226261d524c1e3304c1425187905
liaosi <liaosi@citicsf.com>
2024-07-01 13:41:56 +0800

    python语法。迭代器、生成器和装饰器

---
+++ python_base/Base10_IteratorAndGenerator.py
@@ -0,0 +1,123 @@
+# ================迭代器和生成器================
+
+# 1.迭代器
+'''
+(1)什么是迭代？
+迭代是一个遍历数据集合元素的过程。从第一个元素开始访问，一次只访问一个元素，直到最后一个元素访问完后迭代结束。
+在Python中，迭代通常用于遍历序列（如列表、元组）或任何可迭代对象。并且在python中迭代只能往前，不能往后。
+'''
+# 示例：一个遍历列表的典型方式
+fruits = ['apple', 'banana', 'cherry']
+for fruit in fruits:
+    print("简单for循环遍历：" + fruit)
+
+'''
+(2)迭代器
+迭代器是python中实现迭代功能的工具。有两个基本的方法：iter() 和 next()。iter()方法用于返回一个迭代器对象，而next()方法则返回迭代器的下一个值。当没有更多的值可返回时，next()方法会抛出StopIteration异常。
+'''
+list=[1,2,3,4]
+it_01 = iter(list)    # 创建迭代器对象
+
+print(next(it_01))     # 1
+print(next(it_01))     # 2
+print(next(it_01))     # 3
+
+
+# 2.内置迭代器
+'''
+Python的许多内置类型和函数返回迭代器，例如range()、enumerate()、filter()、map()等。
+'''
+# 示例：使用range()返回迭代器
+for i in range(5):
+    print("range()迭代器遍历：" + str(i))  # 输出: 0, 1, 2, 3, 4
+
+# 示例：使用enumerate()返回迭代器
+# enumerate()函数将可迭代对象转换为一个枚举对象，同时提供索引和值。这在处理需要跟踪索引的情况中非常有用。
+fruits = ['apple', 'banana', 'cherry']
+for index, fruit in enumerate(fruits):
+    print("enumerate()迭代器遍历：" + f"Index: {index}, Fruit: {fruit}")
+
+# 示例：使用filter()返回迭代器
+# filter()可以对可迭代对象的元素进行过滤。例如，过滤出偶数：
+numbers = [1, 2, 3, 4, 5, 6]
+even_numbers = filter(lambda x: x % 2 == 0, numbers)
+for num in even_numbers:
+    print("filter()迭代器遍历：" + str(num))
+
+
+# 示例：使用map()返回迭代器
+# map()函数可以对可迭代对象的元素进行映射操作。例如，映射平方操作：
+squared = map(lambda x: x ** 2, numbers)
+for square in squared:
+    print("map()迭代器遍历：" + str(square))
+
+
+# 3.自定义迭代器
+'''
+自定义迭代器允许我们创建自己的数据结构并以迭代方式访问其内容。在Python中，最常见的方式是通过生成器函数来实现。另外也可以直接定义类来实现__iter__和__next__方法，实现了这两个方法之后，对象可以进行迭代访问。
+'''
+# 示例：直接定义类实现__iter__和__next__方法
+class NumberSequenceIterator:
+    def __init__(self, start, end):
+        self.current = start
+        self.end = end
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.current > self.end:
+            raise StopIteration
+        result = self.current
+        self.current += 1
+        return result
+
+# 使用自定义迭代器
+seq_iter = NumberSequenceIterator(1, 5)
+print(next(seq_iter))
+print(next(seq_iter))
+for num in seq_iter:
+    print("直接定义类实现__iter__和__next__方法：" + str(num))
+
+# 3.1 生成器
+'''
+在python中，yield 是一个关键字，用于定义生成器函数，生成器函数是一种特殊的函数，该函数的返回值是一个生成器（generator），而不是一次性返回一个包含所有结果的数据集合。我们可以通过next()方法或者for循环（也即是迭代的方法）从生成器中依次取出数据集合。
+
+yield关键字有语句暂停和恢复函数执行的作用，迭代时，会在yield语句暂停，并返回有yield修饰的变量或表达式的值，在下次迭代时，会从上次暂停的位置继续执行（即执行yeild语句的下一行代码），直到再次遇到 yield 语句。
+简要理解：执行 yield 语句就是 return 一个值，并且记住这个位置，下次迭代就从这个位置后(下一行)开始。
+'''
+def countdown(n):
+    while n > 0:
+        yield n
+        n -= 1
+ 
+# 创建生成器对象
+generator = countdown(5)
+ 
+# 通过next()获取集合中的下一个值
+print(next(generator))  # 输出: 5
+print(next(generator))  # 输出: 4
+print(next(generator))  # 输出: 3
+ 
+# 使用 for 循环迭代生成器
+for value in generator:
+    print("通过生成器遍历：" + str(value))  # 输出: 2 1
+
+# 3.2 生成器表达式
+'''
+除了生成器函数，Python还提供了生成器表达式，它是一种简洁的创建生成器的方式，类似于列表推导式，但不会立即计算所有结果：
+'''
+squares = (x**2 for x in range(10))
+for square in squares:
+    print("生成器表达式创建的生成器遍历：" + str(square))
+
+
+# 4.迭代器的优缺点
+'''
+(1)优点
+迭代器最大的优点之一是其惰性计算特性。这意味着它不会一次性生成所有数据，而是在需要时按需生成，所以内存开销较小。这对于处理大数据集或无限序列特别有用，因为它们只占用有限的内存。
+(2)缺点
+迭代器不支持反向迭代，这在某些情况下可能会成为限制。
+迭代器通常在第一次迭代后不再可用，因此需要谨慎处理。如果需要多次迭代，应确保每次迭代都有新的迭代器。
+在多线程环境中，迭代器需要额外的同步措施，以防止数据竞争。
+'''
---
+++ python_base/Base11_Decorator.py
@@ -0,0 +1,182 @@
+# ================装饰器================
+"""
+迭代器、生成器和装饰器是python中的3个重要功能，其中装饰器属于较难的一个内容。
+
+什么是装饰器？装饰器可以做什么？
+要理解python中的装饰器，可以参考设计模式中的装饰器模式，装饰器模式的作用是在开闭原则的基础上，对已有功能进行功能扩展。比如系统已有一个功能是生成各种图形（比如圆形、长方形、扇形），现在有个需求是要对生成的图形增加红色的边界，那就可以使用装饰器模式来改造。再比如现在有一些业务处理过程，现在想记录这些过程处理的时间，在Java项目中，可以通过切面等方法来实现，在python中，则可以通过装饰器来实现。
+
+总结：在Python中，装饰器本质上是一个函数，它可以让其他函数在不需要做任何代码变动的前提下增加额外功能，装饰器的返回值也是一个函数对象。它经常用于有切面需求的场景，比如：插入日志、性能测试、事务处理、缓存、权限校验等场景。装饰器是解决这类问题的绝佳设计，有了装饰器，我们就可以抽离出大量与函数功能本身无关的雷同代码并继续重用。
+概括的讲，装饰器的作用就是允许在不修改原有代码的基础上，为已经存在的函数或对象添加额外的功能。
+
+"""
+
+# 1.装饰器的写法
+'''
+在python中装饰器怎么编写呢？
+首先，装饰器分为函数装饰器和类装饰器。函数装饰器是一个函数，它接受一个函数（例如func）作为参数，并返回一个内部函数（例如wrapper），在这个内部函数（指wrapper）内部，你可以执行一些额外的操作，然后调用原始函数（指func），并返回其结果。
+'''
+# 1.1 装饰器的早期写法
+# 在早期 (Python版本 < 2.4，2004年以前)，函数装饰器的写法如下。
+# 示例：为一个已有的方法，增加日志打印功能
+def debug_log(func):
+    def wrapper():
+        print("[DEBUG]: enter {}".format(func.__name__))
+        return func()
+    return wrapper
+ 
+def say_hello():
+    print("hello python!")
+ 
+say_hello1 = debug_log(say_hello)    # 添加功能并保持原函数名不变
+say_hello1()                         # 调用装饰后的方法
+
+
+# 1.2 装饰器的语法糖写法
+# 因为早期写法不够优雅，所以后期版本中，支持语法糖写法，有点像java中的注解。
+@debug_log
+def say_hello2():
+    print("hello java!")
+
+say_hello2()
+
+
+# 2. 带参数的装饰器
+# 2.1 如果被装饰的函数需要传入参数，那装饰器函数要怎么写呢？一个简单的做法指定装饰器函数wrapper接受和原函数一样的参数。比如：
+print("*******************2. 带参数的装饰器*******************示例1")
+def debug_log2(func):
+    def wrapper(something):  # 指定一样的参数
+        print("[DEBUG]: enter {}".format(func.__name__))
+        return func(something)
+    return wrapper  # 装饰器函数返回包装过的函数
+ 
+@debug_log2
+def say(something):
+    print("hello {}!".format(something))
+
+say("hadoop")
+
+# 2.2 但是函数有千千万万，不同的函数的参数不尽相同，想要有通用的传参方法，需要用到python里的可变参数*args和关键字参数**kwargs，有了这两个参数，装饰器就可以用于任意目标函数了。
+print("*******************2. 带参数的装饰器*******************示例2")
+def debug_log3(func):
+    def wrapper(*args, **kwargs):  
+        print("[DEBUG]: enter {}()".format(func.__name__))
+        print('Prepare and say...',)
+        return func(*args, **kwargs)
+    return wrapper  # 返回
+ 
+@debug_log3
+def say2(something):
+    print("hello {}!".format(something))
+
+say2("oracle")
+
+# 2.3 如果上面的日志装饰器不仅需要在进入某个函数后打出日志信息，而且还需要能够指定log的级别，那么装饰器则需要这样写。
+print("*******************2. 带参数的装饰器*******************示例3")
+def logging(level):
+    def wrapper(func):
+        def inner_wrapper(*args, **kwargs):
+            print("[{level}]: enter function {func}".format(level=level, func=func.__name__))
+            return func(*args, **kwargs)
+        return inner_wrapper
+    return wrapper
+ 
+@logging(level='INFO')          # 将日志级别通过参数名方法传入装饰器函数
+def say3(something):
+    print("say3 {}!".format(something))
+ 
+# 如果没有使用@语法，等同于
+# say = logging(level='INFO')(say)
+ 
+@logging(level='DEBUG')
+def do(something):
+    print("do {}...".format(something))
+ 
+say3('hello')
+do("my work")
+
+
+# 3. 类装饰器
+'''
+除了函数装饰器，还有类装饰器。如果让类的构造函数__init__()接受一个函数，然后重载__call__()方法并返回一个函数，就实现了类装饰器。
+'''
+print("*******************3. 类装饰器*******************示例1")
+class LogClass():
+    def __init__(self, func):
+        self.func = func
+ 
+    def __call__(self, *args, **kwargs):
+        print("[DEBUG]: enter function {func}".format(func=self.func.__name__))
+        return self.func(*args, **kwargs)
+
+@LogClass
+def say4(something):
+    print("say4 {}!".format(something))
+
+say4("javascript")
+
+
+# 如果需要在类装饰器实现参数的传入，则需要在__init__()方法中将参数传入。而函数的传入不再从__init__()方法传入，要在重载__call__方法传入函数，并且返回包装过的函数。
+# 示例：比如传入上文中的日志级别：
+print("*******************3. 类装饰器*******************示例2")
+class LogClass2():
+    def __init__(self, level='DEBUG'):
+        self.level = level
+    
+    def __call__(self, func):
+        def wrapper(*args, **kwargs):
+            print("[{level}]: enter function {func}".format(level=self.level, func=func.__name__))
+            func(*args, **kwargs)
+        return wrapper
+
+@LogClass2(level='INFO')
+def say5(something):
+    print("say5 {}!".format(something))
+
+say5("C#")
+
+
+# 4.多个装饰器的顺序
+'''
+一个函数可以被多个装饰器进行装饰，那么装饰器的执行顺序是怎么样的呢？执行一下下面的代码就清楚了
+'''
+print("*******************4.多个装饰器的顺序*******************示例1")
+def decorator_1(func):
+    def wrapper(*args, **kwargs):
+        print('我是装饰器1')
+        func(*args, **kwargs)
+    return wrapper
+ 
+def decorator_2(func):
+    def wrapper(*args, **kwargs):
+        print('我是装饰器2')
+        func(*args, **kwargs)
+    return wrapper
+ 
+def decorator_3(func):
+    def wrapper(*args, **kwargs):
+        print('我是装饰器3')
+        func(*args, **kwargs)
+    return wrapper
+
+@decorator_1
+@decorator_2
+@decorator_3
+def hello3():
+    print("multi decorator test")
+
+hello3()
+
+'''
+输出结果：
+我是装饰器1
+我是装饰器2
+我是装饰器3
+multi decorator test
+
+原因：上面多装饰器语法糖可以看作如下代码，hello3 = decorator_1(decorator_2(decorator_3(hello3)))，由里到外依次执行装饰器的内容。
+'''
+
+# 4.内置的装饰器
+'''
+内置的装饰器和普通的装饰器原理是一样的，只不过返回的不是函数，而是类对象，所以更难理解一些。
+'''
